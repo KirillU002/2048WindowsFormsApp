3a9d878068ead0a5b3598734672c92fc831acbef
Kirill Ukolov <kirill002@bk.ru>
2021-11-15 22:27:39 +0300

    2048

--- 2048WindowsFormsApp/Form1.Designer.cs
+++ 2048WindowsFormsApp/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Счет:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(53, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(13, 13);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "2048";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreLabel;
     }
 }
 
--- 2048WindowsFormsApp/Form1.cs
+++ 2048WindowsFormsApp/Form1.cs
@@ -12,9 +12,269 @@ namespace _2048WindowsFormsApp
 {
     public partial class Form1 : Form
     {
+        private const int mapSize = 4;
+        private Label[,] labelsMap;
+        private static Random random = new Random();
+        private int score = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitMap();
+            GenerateNumber();
+            ShowScore();
+        }
+
+        private void ShowScore()
+        {
+            scoreLabel.Text = score.ToString();
+        }
+
+        private void InitMap()
+        {
+            labelsMap = new Label[mapSize, mapSize];
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLabel = CreateLabel1(i,j);
+                    Controls.Add(newLabel);
+                    labelsMap [i,j] = newLabel;
+                }
+            }
+        }
+
+        private void GenerateNumber()
+        {
+            while (true)
+            {
+                var randomNumberLable = random.Next(mapSize * mapSize);
+                var indexRow = randomNumberLable / mapSize; //i*4+j=number
+                var indexColumn = randomNumberLable % mapSize;//j
+
+                if (labelsMap[indexRow, indexColumn].Text == String.Empty)
+                {
+                    Random rnd = new Random();
+                    if (rnd.Next(1, 5) != 4)
+                        labelsMap[indexRow, indexColumn].Text = "2";
+                    else
+                        labelsMap[indexRow, indexColumn].Text = "4";
+                    break;
+                }
+            }
+        }
+
+        private Label CreateLabel1(int indexRow, int indexColumn)
+        {
+            var label = new Label();//вызываем через конструктор
+            label.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            label.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            label.Size = new System.Drawing.Size(70,70);
+
+            int x = 10 + indexColumn * 76;
+            int y = 70 + indexRow * 76;
+            label.Location = new System.Drawing.Point(x, y);
+            return label;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Right":
+                    for (int i = 0; i < mapSize; i++)
+                    {
+                        for (int j = mapSize-1; j >= 0; j--)
+                        {
+                            if(labelsMap[i,j].Text != string.Empty)
+                            {
+                                for (int k = j-1; k >= 0; k--)//левее от ячейки
+                                {
+                                    if(labelsMap[i,k].Text != string.Empty)
+                                    {
+                                        if(labelsMap[i, j].Text== labelsMap[i, k].Text)
+                                        {
+                                            var number = int.Parse(labelsMap[i, j].Text);
+                                            score += number * 2;
+                                            labelsMap[i, j].Text = (number * 2).ToString();
+                                            labelsMap[i, k].Text = string.Empty;
+                                        }
+                                        break;//2 4 2 чтобы не схлопывались 2 с 2
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < mapSize; i++)
+                    {
+                        for (int j = mapSize - 1; j >= 0; j--)
+                        {
+                            if (labelsMap[i, j].Text == string.Empty)
+                            {
+                                for (int k = j - 1; k >= 0; k--)//левее от ячейки
+                                {
+                                    if (labelsMap[i, k].Text != string.Empty)
+                                    {
+                                            labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                            labelsMap[i, k].Text = string.Empty;
+                                        break;//2 4 2 двигаем
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    GenerateNumber();
+                    ShowScore();
+                    break;
+
+                case "Left":
+                    for (int i = 0; i < mapSize; i++)
+                    {
+                        for (int j = 0; j < mapSize; j++)
+                        {
+                            if (labelsMap[i, j].Text != string.Empty)
+                            {
+                                for (int k = j + 1; k < mapSize; k++)
+                                {
+                                    if (labelsMap[i, k].Text != string.Empty)
+                                    {
+                                        if (labelsMap[i, j].Text == labelsMap[i, k].Text)
+                                        {
+                                            var number = int.Parse(labelsMap[i, j].Text);
+                                            score += number * 2;
+                                            labelsMap[i, j].Text = (number * 2).ToString();
+                                            labelsMap[i, k].Text = string.Empty;
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    for (int i = 0; i < mapSize; i++)
+                    {
+                        for (int j = 0; j < mapSize; j++)
+                        {
+                            if (labelsMap[i, j].Text == string.Empty)
+                            {
+                                for (int k = j + 1; k < mapSize; k++)
+                                {
+                                    if (labelsMap[i, k].Text != string.Empty)
+                                    {
+                                        labelsMap[i, j].Text = labelsMap[i, k].Text;
+                                        labelsMap[i, k].Text = string.Empty;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    GenerateNumber();
+                    ShowScore();
+                    break;
+
+                case "Up":
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        for (int i = 0; i < mapSize; i++)//строка
+                        {
+                            if (labelsMap[i, j].Text != string.Empty)
+                            {
+                                for (int k = i + 1; k < mapSize; k++)//за строки отвечает i. строка
+                                {
+                                    if (labelsMap[k,j].Text != string.Empty)
+                                    {
+                                        if (labelsMap[i, j].Text == labelsMap[k,j].Text)
+                                        {
+                                            var number = int.Parse(labelsMap[i, j].Text);
+                                            score += number * 2;
+                                            labelsMap[i, j].Text = (number * 2).ToString();
+                                            labelsMap[k,j].Text = string.Empty;
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        for (int i = 0; i < mapSize; i++)
+                        {
+                            if (labelsMap[i, j].Text == string.Empty)
+                            {
+                                for (int k = i+ 1; k < mapSize; k++)
+                                {
+                                    if (labelsMap[k,j].Text != string.Empty)
+                                    {
+                                        labelsMap[i, j].Text = labelsMap[k,j].Text;
+                                        labelsMap[k,j].Text = string.Empty;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    GenerateNumber();
+                    ShowScore();
+                    break;
+
+                case "Down":
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        for (int i = mapSize - 1; i >= 0; i--)
+                        {
+                            if (labelsMap[i, j].Text != string.Empty)
+                            {
+                                for (int k = i - 1; k >= 0; k--)
+                                {
+                                    if (labelsMap[k, j].Text != string.Empty)
+                                    {
+                                        if (labelsMap[i, j].Text == labelsMap[k, j].Text)
+                                        {
+                                            var number = int.Parse(labelsMap[i, j].Text);
+                                            score += number * 2;
+                                            labelsMap[i, j].Text = (number * 2).ToString();
+                                            labelsMap[k, j].Text = string.Empty;
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    for (int j = 0; j < mapSize; j++)
+                    {
+                        for (int i = mapSize-1; i>=0; i--)
+                        {
+                            if (labelsMap[i, j].Text == string.Empty)
+                            {
+                                for (int k = i - 1; k >= 0; k--)
+                                {
+                                    if (labelsMap[k, j].Text != string.Empty)
+                                    {
+                                        labelsMap[i, j].Text = labelsMap[k, j].Text;
+                                        labelsMap[k, j].Text = string.Empty;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    GenerateNumber();
+                    ShowScore();
+                    break;
+
+            }
         }
     }
 }
